154e99e32795f9c5553f0c1f01bfddbfb03f302b
Raghavendra Singh <raghavendrasingh989@gmail.com>
2023-03-09 21:40:09 +0530

    Add new list when pressing enter

--- index.js
+++ index.js
@@ -1,84 +1,78 @@
-const qs = function(identifier)
-{
+const qs = function (identifier) {
   return document.querySelector(`${identifier}`);
 };
-const qsa = function(identifier)
-{
+const qsa = function (identifier) {
   return document.querySelectorAll(`${identifier}`);
 };
 // App Setup
 const data = {
-  "personal": [
-    "Personal Task 1", "Personal Task 2", "Personal Task 3"
+  personal: ['Personal Task 1', 'Personal Task 2', 'Personal Task 3'],
+  work: ['Work Task 1', 'Work Task 2', 'Work Task 3'],
+  groceries: [
+    'Groceries Task 1',
+    'Groceries Task 2',
+    'Groceries Task 3',
   ],
-  "work": [
-    "Work Task 1", "Work Task 2", "Work Task 3"
-  ],
-  "groceries": ["Groceries Task 1", "Groceries Task 2", "Groceries Task 3"]
 };
-let selectedList = 'Personal'
+let selectedList = 'Personal';
 let activeList = data.personal;
 
 // Initialize App
-const taskLists = qs(".task-list");
-const populateListOfLists = function()
-{
+const taskLists = qs('.task-list');
+const populateListOfLists = function () {
   taskLists.innerHTML = null;
-  Object.keys(data).forEach((list) =>
-  {
-    let myClass="";
-    if(selectedList == list) myClass+="active-list";
+  Object.keys(data).forEach((list) => {
+    let myClass = '';
+    if (selectedList == list) myClass += 'active-list';
     taskLists.innerHTML += `<li class='list-name ${myClass}'>${list} </li>`;
   });
 };
-const populateActiveList = function() 
-{
-  
-}
+const populateActiveList = function () {};
 populateListOfLists();
 populateActiveList();
 
 // Add New List
 
-const addListBtn = qs("#new-list");
-const newListName = qs("#new-list-name");
+const addListBtn = qs('#new-list');
+const newListName = qs('#new-list-name');
 
-const addList = function(listName)
-{
-  Object.defineProperty(data, listName,
-  {
+const addList = function (listName) {
+  Object.defineProperty(data, listName, {
     value: [],
     writable: true,
-    enumerable: true
+    enumerable: true,
   });
 };
-addListBtn.addEventListener("click", function()
-{
+addListBtn.addEventListener('click', function () {
   addList(newListName.value);
   populateListOfLists(data);
   newListName.value = null;
 });
-
+newListName.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    addListBtn.click();
+  }
+});
 
 // Switch Active List
-const lists = qsa(".list-name");
+const lists = qsa('.list-name');
 
-const removeActiveClassFromListName = function(){
-  lists.forEach(list => {
-    list.classList.remove("active-list");
-  })
-}
+const removeActiveClassFromListName = function () {
+  lists.forEach((list) => {
+    list.classList.remove('active-list');
+  });
+};
 
-const switchActiveList = function()
-{
+const switchActiveList = function () {
   activeList = data[selectedList];
-}
+};
 
-lists.forEach(list =>{
-  list.addEventListener("click", function(e){
+lists.forEach((list) => {
+  list.addEventListener('click', function (e) {
     removeActiveClassFromListName();
-    e.target.classList.add("active-list");
+    e.target.classList.add('active-list');
     selectedList = e.target.innerHTML.trim();
     switchActiveList(e);
-  })
-})
+  });
+});
